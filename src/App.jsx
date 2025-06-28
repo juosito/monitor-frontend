6ec7ea2eb97a37a96d2f97ad4629c7ac46af5040
juosito <juosito@gmail.com>
2025-06-28 19:59:23 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>Monitor de Envíos FLEX</h1>
+      <Link to="/login">Iniciar sesión con Mercado Libre</Link>
+    </div>
   )
 }
-
-export default App
