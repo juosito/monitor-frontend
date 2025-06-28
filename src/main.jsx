bd911a0b7004b5b64b5ea07043b00b35caa5def4
juosito <juosito@gmail.com>
2025-06-28 19:59:07 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/auth" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
 )
